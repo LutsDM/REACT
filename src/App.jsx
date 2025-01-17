dbe371bb687573610b0809224fba00493cedec6f
LutsDM <lutsdm@gmail.com>
2025-01-17 11:51:31 +0100

    first all

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import CityPage from "./components/CityPage/CityPage";
+import Homework01 from "./Homework01/Homework01";
+import Lesson02 from "./lessons/lesson02/Lesson02";
+import Lesson01 from "./lessons/lessons01/Lesson01";
+// eslint-disable-next-line no-unused-vars
+
+
+
+function App() {
+ 
+  return (
+    <>
+      <Lesson02 />
+    </>
+  );
+}
+
+export default App;
