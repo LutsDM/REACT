089cfb9d4402647b90c00fa39d22ae0646612041
LutsDM <lutsdm@gmail.com>
2025-01-19 11:41:07 +0100

    homework move

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable no-unused-vars */
 import "./App.css";
 import CityPage from "./components/CityPage/CityPage";
-import Homework01 from "./Homework01/Homework01";
+import Homework01 from "./homeworks/Homework01/Homework01";
 import Lesson02 from "./lessons/lesson02/Lesson02";
 import Lesson01 from "./lessons/lessons01/Lesson01";
 // eslint-disable-next-line no-unused-vars
@@ -11,7 +12,7 @@ function App() {
  
   return (
     <>
-      <Lesson02 />
+      <Homework01 />
     </>
   );
 }
